bda40b6d62217ba4a8fe73ac58782f39589d65a4
ginsengAttack <howard20020225@gmail.com>
2025-04-13 20:17:09 +0800

    Add task switching in user space for concurrent
    Implement cooperative task switching to process game 1 and
    keyboard events in parallel.
    Store three game tasks and a keyboard handler in an array, managed by
    a scheduler for efficient task execution.

--- xo-user.c
+++ xo-user.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <getopt.h>
+#include <setjmp.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,10 +16,22 @@
 #define XO_DEVICE_FILE "/dev/kxo"
 #define XO_DEVICE_ATTR_FILE "/sys/class/kxo/kxo/kxo_state"
 
-static char table[N_GRIDS];
-// static char draw;
+struct task {
+    jmp_buf env;
+    // char *task_name;
+};
 
-// static int table_initial() {}
+/*for schedule to save status*/
+jmp_buf schedule_buf;
+
+struct task *tasklist[3];
+
+static bool read_attr, end_attr;
+fd_set readset;
+int device_fd;
+int max_fd;
+
+static char table[N_GRIDS];
 
 static int draw_board(char *table)
 {
@@ -78,7 +91,6 @@ static void raw_mode_enable(void)
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-static bool read_attr, end_attr;
 
 static void listen_keyboard_handler(void)
 {
@@ -109,6 +121,114 @@ static void listen_keyboard_handler(void)
     close(attr_fd);
 }
 
+int current = -1;
+
+/*first game include AI1 and AI2*/
+static void task1(void)
+{
+    if (setjmp(tasklist[0]->env) == 0)
+        longjmp(schedule_buf, 1);
+
+    FD_ZERO(&readset);
+    FD_SET(STDIN_FILENO, &readset);
+    FD_SET(device_fd, &readset);
+
+    int result = select(max_fd + 1, &readset, NULL, NULL, NULL);
+    if (result < 0) {
+        printf("Error with select system call\n");
+        exit(1);
+    }
+
+    char step[5];
+
+    if (read_attr && FD_ISSET(device_fd, &readset)) {
+        step[4] = '\0';
+        FD_CLR(device_fd, &readset);
+
+        read(device_fd, step, 4);
+        printf("\033[H\033[J"); /* ASCII escape code to clear the screen */
+        table[step[1] - '0'] = step[0];
+        draw_board(table);
+
+        if (step[2] == 'W') {
+            printf("%c win!!!\n", step[0]);
+            memset(table, ' ',
+                   N_GRIDS); /* Reset the table so the game restart */
+            // read(device_fd, step, 4);
+            memset(step, ' ', 4); /* Reset the table so the game restart */
+        }
+    }
+
+    longjmp(schedule_buf, 1);
+}
+static void task2(void)
+{
+    if (setjmp(tasklist[1]->env) == 0)
+        longjmp(schedule_buf, 1);
+
+    longjmp(schedule_buf, 1);
+}
+static void keyboard(void)
+{
+    if (setjmp(tasklist[2]->env) == 0)
+        longjmp(schedule_buf, 1);
+
+    if (FD_ISSET(STDIN_FILENO, &readset)) {
+        FD_CLR(STDIN_FILENO, &readset);
+        listen_keyboard_handler();
+    }
+    longjmp(schedule_buf, 1);
+}
+
+/*initial tasklist and add task1 task2 into it*/
+static void initial_tasklist(void)
+{
+    struct task *task_1 = malloc(sizeof(struct task));
+    struct task *task_2 = malloc(sizeof(struct task));
+    struct task *key_board = malloc(sizeof(struct task));
+
+    // task1->task_name = strdup("task1");
+    // task2->task_name = strdup("task2");
+    // keyboard->task_name = strdup("keyboard");
+
+    tasklist[0] = task_1;
+    tasklist[1] = task_2;
+    tasklist[2] = key_board;
+}
+
+/*the function to switch task*/
+static void switch_task()
+{
+    current %= 3;
+    longjmp(tasklist[current]->env, 1);
+}
+/**/
+int n = 3;
+static void schedule(void)
+{
+    initial_tasklist();
+    setjmp(schedule_buf);
+
+    if (n != 0) {
+        if (n == 3) {
+            n--;
+            task1();
+        } else if (n == 2) {
+            n--;
+            task2();
+        } else {
+            n--;
+            keyboard();
+        }
+    }
+
+    if (end_attr)
+        return;
+
+    current++;
+    switch_task();
+}
+
 int main(int argc, char *argv[])
 {
     if (!status_check())
@@ -118,47 +238,17 @@ int main(int argc, char *argv[])
     int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
     fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
 
-    // char display_buf[DRAWBUFFER_SIZE];
-    char step[5];
 
-    fd_set readset;
-    int device_fd = open(XO_DEVICE_FILE, O_RDONLY);
-    int max_fd = device_fd > STDIN_FILENO ? device_fd : STDIN_FILENO;
+    // fd_set readset;
+    device_fd = open(XO_DEVICE_FILE, O_RDONLY);
+    max_fd = device_fd > STDIN_FILENO ? device_fd : STDIN_FILENO;
     read_attr = true;
     end_attr = false;
 
-    while (!end_attr) {
-        FD_ZERO(&readset);
-        FD_SET(STDIN_FILENO, &readset);
-        FD_SET(device_fd, &readset);
+    schedule();
 
-        int result = select(max_fd + 1, &readset, NULL, NULL, NULL);
-        if (result < 0) {
-            printf("Error with select system call\n");
-            exit(1);
-        }
-
-        if (FD_ISSET(STDIN_FILENO, &readset)) {
-            FD_CLR(STDIN_FILENO, &readset);
-            listen_keyboard_handler();
-        } else if (read_attr && FD_ISSET(device_fd, &readset)) {
-            step[4] = '\0';
-            FD_CLR(device_fd, &readset);
-
-            read(device_fd, step, 4);
-            printf("\033[H\033[J"); /* ASCII escape code to clear the screen */
-            table[step[1] - '0'] = step[0];
-            draw_board(table);
-
-            if (step[2] == 'W') {
-                printf("%c win!!!\n", step[0]);
-                memset(table, ' ',
-                       N_GRIDS); /* Reset the table so the game restart */
-                read(device_fd, step, 4);
-                memset(step, ' ', 4); /* Reset the table so the game restart */
-            }
-        }
-    }
+    for (int i = 0; i < 2; i++)
+        free(tasklist[i]);
 
     raw_mode_disable();
     fcntl(STDIN_FILENO, F_SETFL, flags);
